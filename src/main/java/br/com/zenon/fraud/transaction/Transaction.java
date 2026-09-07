b566ecec9d678820efbb1426b6a7502f9340c67d
Douglas <douglasmodolo@outlook.com.br>
2026-09-07 15:35:46 -0300

    feat: modela transações do PaySim com records (tarefa 02)

---
+++ src/main/java/br/com/zenon/fraud/transaction/Transaction.java
@@ -0,0 +1,13 @@
+package br.com.zenon.fraud.transaction;
+
+import java.math.BigDecimal;
+
+public record Transaction(
+    int step,
+    TransactionType type,
+    BigDecimal amount,
+    TransactionCustomer origin,
+    TransactionCustomer recipient,
+    boolean isFraud,
+    boolean isFlaggedFraud
+) { }
---
+++ src/main/java/br/com/zenon/fraud/transaction/TransactionType.java
@@ -0,0 +1,5 @@
+package br.com.zenon.fraud.transaction;
+
+public enum TransactionType {
+    CASH_IN, CASH_OUT, DEBIT, PAYMENT, TRANSFER
+}
